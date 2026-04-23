d480297bdaf1e535c8fee1db6883093c8cf3f37f
Leo <40034404+NeoLeonn3@users.noreply.github.com>
2026-04-23 22:35:03 +0300

    more deploy.yml changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/champdex/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/champdex/' : '/',
   plugins: [react(), tailwindcss()],
-})
+}))
